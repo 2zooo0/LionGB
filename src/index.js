292b911ec90bdbb3c43f60f7e3781740fcd09fda
2zooo0 <c20221460@cau.ac.kr>
2024-06-04 21:11:18 +0900

    버튼 타입 수정함

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import { newData } from './newG.js';
 import { getData } from './getG.js';
 import { delData } from './delG.js';
 
-window.onload = getData();
+//window.onload = getData();
 
 const submitbtn = document.getElementById('submitbtn');
 document.getElementById('submitbtn').addEventListener('click', newData);
+
+const getGbtn = document.getElementById('getGbtn');
+document.getElementById('getGbtn').addEventListener('click', getData );
